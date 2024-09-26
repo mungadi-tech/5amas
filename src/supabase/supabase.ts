4f9b0e771de8f028d0575ac2891777486dae659c
ayubaezekiel <ezekielayoba@yahoo.com>
2024-09-26 12:55:28 +0100

    fixed env

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../lib/supabase.types";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON;
 
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
